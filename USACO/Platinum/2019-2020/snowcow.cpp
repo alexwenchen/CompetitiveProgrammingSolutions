c59589eab5d12d9faeb8eb5644438c576ebd7e6a
UsersBad <47618496+UsersBad@users.noreply.github.com>
2020-12-07 14:38:39 -0600

    Update snowcow.cpp

--- USACO/Platinum/2019-2020/snowcow.cpp
+++ USACO/Platinum/2019-2020/snowcow.cpp
@@ -1,3 +1,4 @@
+//cheesed it before, this is realsolve
 //#pragma GCC optimize("Ofast")
 //#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
 //#pragma GCC optimization ("unroll-loops")
